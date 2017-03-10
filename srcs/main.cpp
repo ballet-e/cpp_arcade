8611e6d7bc02cb7905723db6c6732a4c4581a354
Arnaud WURMEL <arnaud.wurmel@epitech.eu>
2017-03-10 17:02:05 +0100

    start main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,13 +5,24 @@
 // Login   <wurmel_a@epitech.net>
 // 
 // Started on  Thu Mar  9 16:27:05 2017 Arnaud WURMEL
-// Last update Thu Mar  9 16:34:18 2017 Arnaud WURMEL
+// Last update Fri Mar 10 16:56:23 2017 Arnaud WURMEL
 //
 
 #include <iostream>
 
+static void	display_ascii(void)
+{
+  std::cout << "    ___                        __   " << std::endl;
+  std::cout << "   /   |  ______________ _____/ /__ " << std::endl;
+  std::cout << "  / /| | / ___/ ___/ __ `/ __  / _ \\" << std::endl;
+  std::cout << " / ___ |/ /  / /__/ /_/ / /_/ /  __/" << std::endl;
+  std::cout << "/_/  |_/_/   \\___/\\__,_/\\__,_/\\___/ " << std::endl;
+  std::cout << "\033[0m" << std::endl;
+}
+
 int	main(void)
 {
-  std::cout << "[Arcade] Loading..." << std::endl;
+  display_ascii();
+  std::cerr << "[Arcade] Loading...\t\033[32mOK\033[0m" << std::endl;
   return 0;
 }
