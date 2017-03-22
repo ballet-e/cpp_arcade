ccf7e534f632614e0fefb063812ad6aabb492f95
Arnaud WURMEL <arnaud.wurmel@epitech.eu>
2017-03-22 16:30:22 +0100

    fix 2.3

--- lib/libsfml/SFMLWrapper/SFMLWrapper.cpp
+++ lib/libsfml/SFMLWrapper/SFMLWrapper.cpp
@@ -5,7 +5,7 @@
 // Login   <wurmel_a@epitech.net>
 // 
 // Started on  Sat Mar 11 22:36:02 2017 Arnaud WURMEL
-// Last update Thu Mar 16 14:53:17 2017 Arnaud WURMEL
+// Last update Wed Mar 22 16:29:46 2017 Arnaud WURMEL
 //
 
 #include <sys/types.h>
@@ -40,16 +40,14 @@ void	Arcade::SFMLWrapper::drawTitle()
   else
     text.setFont(font);
   text.setCharacterSize(50);
-  text.setOutlineColor(sf::Color::Blue);
-  text.setFillColor(sf::Color::Blue);
+  text.setColor(sf::Color::Blue);
   it = to_disp.begin();
   while (it != to_disp.end())
     {
       text.setString("Arcade");
       text.setPosition((600 - (text.getLocalBounds().width / 2)) + ((it - to_disp.begin()) * 52), 10);
       text.setString(*it);
-      text.setOutlineColor(tab[(it - to_disp.begin()) % 5]);
-      text.setFillColor(tab[(it - to_disp.begin()) % 5]);
+      text.setColor(tab[(it - to_disp.begin()) % 5]);
       _window->draw(text);
       ++it;
     }
@@ -58,8 +56,7 @@ void	Arcade::SFMLWrapper::drawTitle()
   else
     text.setString("Choisir un jeu");
   text.setCharacterSize(15);
-  text.setOutlineColor(sf::Color::White);
-  text.setFillColor(sf::Color::White);
+  text.setColor(sf::Color::White);
   text.setPosition(600 - (text.getLocalBounds().width / 2), 120);
   _window->draw(text);
 }
@@ -252,8 +249,7 @@ void	Arcade::SFMLWrapper::setText(std::string const& to_print,
   else if (mode == LEFT_CENTER)
     x = ((_window->getView().getSize().x / 2) - (text.getLocalBounds().width + 50));
   text.setPosition(x, y);
-  text.setOutlineColor(colors[fontColor]);
-  text.setFillColor(colors[fontColor]);
+  text.setColor(colors[fontColor]);
   sf::FloatRect backgroundRect = text.getLocalBounds();
   sf::RectangleShape background(sf::Vector2f(backgroundRect.width + 4, backgroundRect.height + 4));
 
