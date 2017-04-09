9888a90fc1febbe0a44538919420921812f60515
ballet <erwan.ballet@epitech.eu>
2017-04-09 13:49:11 +0200

    pacman.hh

--- games/libpacman/Pacman/Pacman.hh
+++ games/libpacman/Pacman/Pacman.hh
@@ -5,10 +5,53 @@
 // Login   <ballet_e@epitech.net>
 // 
 // Started on  Sun Apr  9 13:29:12 2017 Erwan BALLET
-// Last update Sun Apr  9 13:30:56 2017 Erwan BALLET
+// Last update Sun Apr  9 13:47:48 2017 Erwan BALLET
 //
 
 #ifndef PACMAN_HH_
 # define PACMAN_HH_
+
+# include <utility>
+# include "IGame.hh"
+# include "Event.hh"
+
+namespace	Arcade
+{
+  class IGraphics;
+
+  class Pacman : public Arcade::IGame
+  {
+  public:
+    Pacman();
+    ~Pacman();
+
+  public:
+    void	setUpGraphics(Arcade::IGraphic *);
+    void	setUpPseudo(std::string const&);
+    LibraryType	getLibraryType() const;
+    void	eventListener(Event const&);
+    void	render();
+    bool	shouldRender();
+    void	initGame();
+    unsigned int	getMapWidth() const;
+    unsigned int	getMapHeight() const;
+    Arcade::IGame::GameState	gameState() const;
+
+  public:
+    void	whereAmI();
+    void	getMap();
+    void	goUp();
+    void	goDown();
+    void	goLeft();
+    void	goRight();
+    void	goForward();
+    void	makeShoot();
+    void	illegal();
+    void	playRound();
+
+  private:
+    IGraphic	*_graphic_library;
+  };
+}
 
 #endif /* !PACMAN_HH_ */
