b96a7359b31ed2e4eda479b7cd9445478ecbb1ce
ballet <erwan.ballet@epitech.eu>
2017-04-09 13:31:39 +0200

    start rush pacman

---
+++ games/libpacman/Pacman/Pacman.hh
@@ -0,0 +1,14 @@
+//
+// Pacman.hh for  in /home/ballet_e/CPP/cpp_arcade/games/libpacman/Pacman
+// 
+// Made by Erwan BALLET
+// Login   <ballet_e@epitech.net>
+// 
+// Started on  Sun Apr  9 13:29:12 2017 Erwan BALLET
+// Last update Sun Apr  9 13:30:56 2017 Erwan BALLET
+//
+
+#ifndef PACMAN_HH_
+# define PACMAN_HH_
+
+#endif /* !PACMAN_HH_ */
